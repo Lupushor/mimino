1ea8a781426082a41c57ab4815367807d324ae89
Aleksandr Lupu <alexandr.lupu.1984@gmail.com>
2023-06-06 22:37:30 +0300

    mimino

---
+++ src/index.js
@@ -0,0 +1,20 @@
+$('.section-slider-plugin').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  variableWidth: true,
+  autoplay: true,
+});
+
+$('.hero-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 1500,
+  easing: 'ease',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: false,
+  autoplay: true,
+});
